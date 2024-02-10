7caa659347b31516e457a59842a00a8060b61805
Ferdousfrd <ferdous.frd81@gmail.com>
2024-02-10 16:10:09 +0200

    Added new function that reverse the game and let computer guess

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import random
 
-def guessGame(range):
+def userGuessGame(range):
     random_number = random.randint(1, range)
     guess = 0
 
@@ -14,4 +14,19 @@ def guessGame(range):
             print(f"You have given, {guess}. And random number is bigger than this. Go higher!")
     print(f"Great! You got it right! The random number was {random_number}")
 
-guessGame(10)
+def computerGuessing(x):
+    low = 1
+    high = x
+    decision = ""
+    while decision != "c":
+        guess = random.randint(low, high)
+        decision = input(f"PC guessed, {guess}. If correct, press 'C', if it guessed high then press 'H' and if low then press 'L'__").lower()
+        if decision == "h":
+            high = guess - 1
+        elif decision == "l":
+            low = guess + 1
+
+    print("You have guesses correct!")
+
+
+computerGuessing(10)
